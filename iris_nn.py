17097cf1f4e527ad4f6b12229bb2e712a853a960
ayush1999 <ayush.shridhar1999@gmail.com>
2017-09-30 22:56:00 +0530

    iris using NN

---
+++ iris_nn.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+iris = load_iris()
+x = iris.data
+y = iris.target
+
+mlp = MLPClassifier(hidden_layer_sizes=(100,100,100))
+mlp.fit(x,y)
+
+predictions = mlp.predict(x)
+
+print(predictions)
+
+for i in range(len(predictions)):
+    predictions[i]= predictions[i]- y[i]
+
+print(predictions)
+
