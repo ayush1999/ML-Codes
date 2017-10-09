6f94471c8fd79125ebcc255e6d4c496cbca33360
ayush1999 <ayush.shridhar1999@gmail.com>
2017-10-09 22:05:45 +0530

    keras upload

--- neural_network.py
+++ neural_network.py
@@ -7,20 +7,22 @@ cancer = load_breast_cancer(return_X_y=True)
 x= cancer[0]
 y= cancer[1]
 
-X_train , X_test, Y_train, Y_test = train_test_split(x,y)
+X_train , X_test, Y_train, Y_test = train_test_split(x,y)   # To Check this
 
 scaler = StandardScaler()
 scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+X_train = scaler.transform(X_train)                         # Transforming using
+X_test = scaler.transform(X_test)                           # Feature Scaling
 
-mlp = MLPClassifier(hidden_layer_sizes=(30,30,30))
+mlp = MLPClassifier(hidden_layer_sizes=(2,2,2))
 mlp.fit(X_train,Y_train)
 
 predictions = mlp.predict(X_train)
 print(predictions)
-
+count=0
 for i in range(len(predictions)):
     predictions[i]= predictions[i]-Y_train[i]
-
-print(predictions)
+    if(predictions[i]!=0):
+        count=count+1;
+    
+print(count)
--- svm_implementation.py
+++ svm_implementation.py
@@ -1,7 +1,8 @@
 from sklearn import svm
 from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 iris = load_iris()
-model = svm.SVC(kernel = 'linear', gamma=1)
+model = svm.SVC(gamma=1)
 x= iris.data[0:-1]
 y = iris.target[0:-1]
 model.fit(x,y)
