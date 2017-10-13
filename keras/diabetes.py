a6d01000bdb9332a91c066de2b7c9677ef89ce69
ayush1999 <ayush.shridhar1999@gmail.com>
2017-10-13 15:30:08 +0530

    ConvNets Added

--- keras/diabetes.py
+++ keras/diabetes.py
@@ -18,16 +18,11 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x,y,epochs=1000, batch_size=10)
+model.fit(x,y,epochs=10, batch_size=10)
 
 scores = model.evaluate(x,y)
 print('{} and {}'.format(model.metrics_names[1],scores[1]*100))
 
 predictions = model.predict(x)
-rounded = [round(x[0]) for x in predictions]
-count = 0
-for i in range(len(rounded)):
-    if rounded[i]!= y[i]:
-        count= count+1
-print(rounded)
-print('Number of incorrect answers are {}'.format(count))
+print(predictions[0])
+print(predictions)
